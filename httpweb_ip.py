ed8aa938070bd33d0d59ab9e0541d8dd2cb6ba67
SilentDox <158617930+SilentDox@users.noreply.github.com>
2024-02-02 23:56:53 +0300

    Update httpweb_ip.py

--- httpweb_ip.py
+++ httpweb_ip.py
@@ -10,7 +10,7 @@ from functools import lru_cache
 # Проверка по IP
 class HttpWebIp:
     """Информация получаемая с сайта httpweb.ru и httpbin.ru никак\n
-    не синхронизирован/связан с noblack-mail а так-же с noblack.command.
+    не синхронизирован/связан с silent а так-же с silentDOX.
     :param: `user_number` - Номер телефона."""
 
 
@@ -76,7 +76,7 @@ class HttpWebIp:
             
             # Получение разрешении на просмотр IP пользователя
             console_clear()
-            _user_permission = str = input(f'{COLOR_CODE["CYAN"]}{COLOR_CODE["BOLD"]}[{COLOR_CODE["RED"]}?{COLOR_CODE["CYAN"]}] {COLOR_CODE["LI_G"]}' + 
+            _user_permission = str = input(f'{COLOR_CODE["BLUE"]}{COLOR_CODE["BOLD"]}[{COLOR_CODE["CYAN"]}?{COLOR_CODE["BLUE"]}] {COLOR_CODE["LI_G"]}' + 
                 f'Разрешите увидеть ваш IP {COLOR_CODE["DARK"]}(Да - 1 / Нет - 0): {COLOR_CODE["RESET"]}').strip()
             
             if not _user_permission or _user_permission == "1":
@@ -87,29 +87,29 @@ class HttpWebIp:
                 _my_ip = self.__not_found_text
             # -----------------------------------------------
             
-            print(f'{COLOR_CODE["CYAN"]}{COLOR_CODE["BOLD"]}[#] {COLOR_CODE["LI_G"]}Ваш IP:' +
+            print(f'{COLOR_CODE["BLUE"]}{COLOR_CODE["BOLD"]}[#] {COLOR_CODE["LI_G"]}Ваш IP:' +
                 f' {COLOR_CODE["DARK"]}{_my_ip}{COLOR_CODE["RESET"]}')
             
-            _user_ip: str = input(f'{COLOR_CODE["CYAN"]}{COLOR_CODE["BOLD"]}[*] {COLOR_CODE["LI_G"]}' + 
+            _user_ip: str = input(f'{COLOR_CODE["BLUEN"]}{COLOR_CODE["BOLD"]}[*] {COLOR_CODE["LI_G"]}' + 
                 f'Введите IP: {COLOR_CODE["RESET"]}').strip()
             
             # Если ввели IP
             if _user_ip:
-                print(f'{COLOR_CODE["RED"]}[~] {COLOR_CODE["YELLOW"]}Поиск данных.. {COLOR_CODE["RESET"]}\n')
+                print(f'{COLOR_CODE["BLUE"]}[~] {COLOR_CODE["CYAN"]}Поиск данных.. {COLOR_CODE["RESET"]}\n')
                 _get_user_ip_data = self.__return_ip_data(user_ip=_user_ip)
 
                 # Проверка статуса ошибки
                 if _get_user_ip_data.get("limit") <= 0:
-                    print(f'\n{COLOR_CODE["RED"]}{COLOR_CODE["BOLD"]}[!] '+
+                    print(f'\n{COLOR_CODE["BLUE"]}{COLOR_CODE["BOLD"]}[!] '+
                         f'{COLOR_CODE["RESET"]}К сожалению, вы израсходовали {COLOR_CODE["DARK"]}все лимиты')
                     
-                    print(f'{COLOR_CODE["RED"]}{COLOR_CODE["BOLD"]}[+] '+
+                    print(f'{COLOR_CODE["BLUE"]}{COLOR_CODE["BOLD"]}[+] '+
                         f'{COLOR_CODE["RESET"]}Всего лимитов: {COLOR_CODE["DARK"]}'+
                         f'{_get_user_ip_data.get("limit", self.__not_found_text)}{COLOR_CODE["RESET"]}')
                 
                 # Проверка статуса ошибки
                 elif _get_user_ip_data.get("status_error") or _get_user_ip_data.get("error"):
-                    print(f'{COLOR_CODE["RED"]}[!] {COLOR_CODE["YELLOW"]}Данные не найдены {COLOR_CODE["RESET"]}\n')
+                    print(f'{COLOR_CODE["BLUE"]}[!] {COLOR_CODE["CYAN"]}Данные не найдены {COLOR_CODE["RESET"]}\n')
 
                 # Ввод IP данных
                 else:
@@ -119,66 +119,66 @@ class HttpWebIp:
                     # Страна
                     # Для украины информация о стране отсутствует
                     if _ip_data_country.get("country_code3") == 'UKR':
-                        print(f'{COLOR_CODE["CYAN"]}{COLOR_CODE["BOLD"]}[+] '+
+                        print(f'{COLOR_CODE["BLUE"]}{COLOR_CODE["BOLD"]}[+] '+
                             f'{COLOR_CODE["LI_G"]}Страна:{COLOR_CODE["F_CL"]} Украина{COLOR_CODE["RESET"]}')
                     
                     else:
-                        print(f'{COLOR_CODE["CYAN"]}{COLOR_CODE["BOLD"]}[+] '+
+                        print(f'{COLOR_CODE["BLUE"]}{COLOR_CODE["BOLD"]}[+] '+
                             f'{COLOR_CODE["LI_G"]}Страна:{COLOR_CODE["F_CL"]} '+
                             f'{_ip_data_country.get("name", self.__not_found_text)}, {_ip_data_country.get("fullname", self.__not_found_text)}{COLOR_CODE["RESET"]}')
 
                     # Код страны
-                    print(f'{COLOR_CODE["CYAN"]}{COLOR_CODE["BOLD"]}[+] '+
+                    print(f'{COLOR_CODE["BLUE"]}{COLOR_CODE["BOLD"]}[+] '+
                         f'{COLOR_CODE["LI_G"]}Код страны:{COLOR_CODE["F_CL"]} '+
                         f'{_ip_data_country.get("country_code3", self.__not_found_text)}{COLOR_CODE["RESET"]}')
                     
                     # Код номеров телефона
-                    print(f'{COLOR_CODE["CYAN"]}{COLOR_CODE["BOLD"]}[+] '+
+                    print(f'{COLOR_CODE["BLUE"]}{COLOR_CODE["BOLD"]}[+] '+
                         f'{COLOR_CODE["LI_G"]}Код номеров:{COLOR_CODE["F_CL"]} '+
                         f'{_ip_data_country.get("telcod", self.__not_found_text)}{COLOR_CODE["RESET"]}')
 
                     # Локация
-                    print(f'{COLOR_CODE["CYAN"]}{COLOR_CODE["BOLD"]}[+] '+
+                    print(f'{COLOR_CODE["BLUE"]}{COLOR_CODE["BOLD"]}[+] '+
                         f'{COLOR_CODE["LI_G"]}Локация:{COLOR_CODE["F_CL"]} '+
                         f'{_ip_data_country.get("location", self.__not_found_text)}{COLOR_CODE["RESET"]}')
 
                     # Разговорный речь (Язык)
-                    print(f'{COLOR_CODE["CYAN"]}{COLOR_CODE["BOLD"]}[+] '+
+                    print(f'{COLOR_CODE["BLUE"]}{COLOR_CODE["BOLD"]}[+] '+
                         f'{COLOR_CODE["LI_G"]}Язык:{COLOR_CODE["F_CL"]} '+
                         f'{_ip_data_country.get("lang", self.__not_found_text)}{COLOR_CODE["RESET"]}')
 
                     # Город (Где используеться номер телефона)
-                    print(f'{COLOR_CODE["CYAN"]}{COLOR_CODE["BOLD"]}[+] '+
+                    print(f'{COLOR_CODE["BLUE"]}{COLOR_CODE["BOLD"]}[+] '+
                         f'{COLOR_CODE["LI_G"]}Город:{COLOR_CODE["F_CL"]} '+
                         f'{_ip_data_region.get("name", self.__not_found_text)}{COLOR_CODE["RESET"]}')
 
                     # Код машин города
-                    print(f'{COLOR_CODE["CYAN"]}{COLOR_CODE["BOLD"]}[+] '+
+                    print(f'{COLOR_CODE["BLUE"]}{COLOR_CODE["BOLD"]}[+] '+
                         f'{COLOR_CODE["LI_G"]}Код машин:{COLOR_CODE["F_CL"]} '+
                         f'{_get_user_ip_data.get("autocod", self.__not_found_text)}{COLOR_CODE["RESET"]}')
                     
                     # Широта
-                    print(f'{COLOR_CODE["CYAN"]}{COLOR_CODE["BOLD"]}[+] '+
+                    print(f'{COLOR_CODE["BLUE"]}{COLOR_CODE["BOLD"]}[+] '+
                         f'{COLOR_CODE["LI_G"]}Широта:{COLOR_CODE["F_CL"]} '+
                         f'{_get_user_ip_data.get("latitude", self.__not_found_text)}{COLOR_CODE["RESET"]}')
 
                     # Долгота
-                    print(f'{COLOR_CODE["CYAN"]}{COLOR_CODE["BOLD"]}[+] '+
+                    print(f'{COLOR_CODE["BLUE"]}{COLOR_CODE["BOLD"]}[+] '+
                         f'{COLOR_CODE["LI_G"]}Долгота:{COLOR_CODE["F_CL"]} '+
                         f'{_get_user_ip_data.get("longitude", self.__not_found_text)}{COLOR_CODE["RESET"]}')
                     
                     # Всего лимитов
-                    print(f'\n{COLOR_CODE["CYAN"]}{COLOR_CODE["BOLD"]}[+] '+
+                    print(f'\n{COLOR_CODE["BLUE"]}{COLOR_CODE["BOLD"]}[+] '+
                         f'{COLOR_CODE["RED"]}Всего лимитов: '+
                         f'{_get_user_ip_data.get("limit", self.__not_found_text)}{COLOR_CODE["RESET"]}')
 
-                    input(f'{COLOR_CODE["CYAN"]}{COLOR_CODE["BOLD"]}[{COLOR_CODE["RED"]}!{COLOR_CODE["CYAN"]}] {COLOR_CODE["LI_G"]}' + 
+                    input(f'{COLOR_CODE["BLUE"]}{COLOR_CODE["BOLD"]}[{COLOR_CODE["CYAN"]}!{COLOR_CODE["BLUE"]}] {COLOR_CODE["LI_G"]}' + 
                         f'Чтобы завершить поиск, нажмите{COLOR_CODE["DARK"]} {COLOR_CODE["RESET"]}ENTER ')
             
             # Если не ввели IP
             else:
-                print(f'{COLOR_CODE["RED"]}[!] {COLOR_CODE["YELLOW"]}Ошибка, введите IP! {COLOR_CODE["RESET"]}\n')
+                print(f'{COLOR_CODE["BLUE"]}[!] {COLOR_CODE["CYAN"]}Ошибка, введите IP! {COLOR_CODE["RESET"]}\n')
 
         except KeyboardInterrupt:
-            print(f'\n{COLOR_CODE["RED"]}[!] {COLOR_CODE["YELLOW"]}Вынужденная остановка работы! {COLOR_CODE["RESET"]}\n')
+            print(f'\n{COLOR_CODE["BLUE"]}[!] {COLOR_CODE["CYAN"]}Вынужденная остановка работы! {COLOR_CODE["RESET"]}\n')
 
