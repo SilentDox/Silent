082a5722a03f2d9ef517ae10ead85747d45e0463
SilentDox <158617930+SilentDox@users.noreply.github.com>
2024-02-03 00:10:54 +0300

    Update noblack_auto.py

--- noblack_auto.py
+++ noblack_auto.py
@@ -1,7 +1,5 @@
 # -*- coding UTF-8 -*-
-# ИНФОРМАЦИЯ БУДЕТ ПРЕДОСТАВЛЕНА ДОЧЕРНЕМУ ПРОЕКТУ NOBLACK-AUTO.RU
-# NOBLACK-MAIL НИКАК НЕ УЧАСТВУЕТ В СОХРАНЕНИИ ПОЛЬЗОВАТЕЛЬСКИХ ДАННЫХ!.
-# DATE: Вторник, 18 Апреля 2023 г. 03:39:15 (+03)
+# SILENT НИКАК НЕ УЧАСТВУЕТ В СОХРАНЕНИИ ПОЛЬЗОВАТЕЛЬСКИХ ДАННЫХ!.
 
 from .config import COLOR_CODE, GLOBAL_SOFT_INFO, console_clear
 
@@ -10,14 +8,14 @@ def print_noblack_auto_text():
     """Показ вспомогательных данных"""
     try:
         console_clear()
-        print(f'{COLOR_CODE["CYAN"]}{COLOR_CODE["BOLD"]}[{COLOR_CODE["RED"]}*{COLOR_CODE["CYAN"]}] {COLOR_CODE["LI_G"]}'
+        print(f'{COLOR_CODE["BLUE"]}{COLOR_CODE["BOLD"]}[{COLOR_CODE["CYAN"]}*{COLOR_CODE["BLUE"]}] {COLOR_CODE["LI_G"]}'
             f'Для проверки автомобилей (Сайт):{COLOR_CODE["RESET"]} {GLOBAL_SOFT_INFO["NOLACK_AUTO_SITE"]}')
 
-        print(f'{COLOR_CODE["CYAN"]}{COLOR_CODE["BOLD"]}[{COLOR_CODE["RED"]}*{COLOR_CODE["CYAN"]}] {COLOR_CODE["LI_G"]}'
+        print(f'{COLOR_CODE["BLUE"]}{COLOR_CODE["BOLD"]}[{COLOR_CODE["CYAN"]}*{COLOR_CODE["BLUE"]}] {COLOR_CODE["LI_G"]}'
             f'Для проверки автомобилей (Телеграмм):{COLOR_CODE["RESET"]} {GLOBAL_SOFT_INFO["NOLACK_AUTO_BOT"]}')
 
-        input(f'{COLOR_CODE["CYAN"]}{COLOR_CODE["BOLD"]}[{COLOR_CODE["RED"]}!{COLOR_CODE["CYAN"]}] {COLOR_CODE["LI_G"]}' + 
+        input(f'{COLOR_CODE["BLUE"]}{COLOR_CODE["BOLD"]}[{COLOR_CODE["CYAN"]}!{COLOR_CODE["BLUE"]}] {COLOR_CODE["LI_G"]}' + 
             f'Чтобы завершить поиск, нажмите{COLOR_CODE["DARK"]} {COLOR_CODE["RESET"]}ENTER ')
 
     except KeyboardInterrupt:
-        print(f'\n{COLOR_CODE["RED"]}[!] {COLOR_CODE["YELLOW"]}Вынужденная остановка работы! {COLOR_CODE["RESET"]}\n')
+        print(f'\n{COLOR_CODE["BLUE"]}[!] {COLOR_CODE["CYAN"]}Вынужденная остановка работы! {COLOR_CODE["RESET"]}\n')
